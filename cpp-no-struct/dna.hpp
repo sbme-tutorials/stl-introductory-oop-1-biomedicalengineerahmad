61f5b5ca92332d7d59b9a0321ca9382a0426151d
biomedicalengineerahmad <ahmedmageed19@gmail.com>
2019-03-28 23:04:47 +0200

    finished no struct , to be continued after midterms

--- cpp-no-struct/dna.hpp
+++ cpp-no-struct/dna.hpp
@@ -3,21 +3,53 @@
 
 namespace dna
 {
+    char complementaryBase( char base )
+    {
+        if (base == 'T')
+        {
+            return 'A' ;
+        }
+        else if (base == 'A')
+        {
+            return 'T' ;
+        }
+        else if (base == 'C')
+        {
+            return 'G' ;
+        }
+        else if (base == 'G')
+        {
+            return 'C' ;
+        }
+        else
+        {
+            return 0 ;
+        }
+    }
+    char *complementarySequence (char *base , int size )
+    {
+        char *compl_sequence= new char [size] ;
+        int index = 0;                                          // A counter for the returned array
+        for (int i = size-1 ; i >= 0 ; --i )                      // An inverted counter for the base
+        {
+            compl_sequence[index] = complementaryBase(base[i]);
+            ++index ;
+        }
+        return compl_sequence ;
 
-char complementaryBase(char base)
-{
-    // Copy from assignment 3
-}
-
-char *complementarySequence(char *base, int size)
-{
-    // Copy from assignment 3
-}
-
-int countChar(char *base, int size, char test)
-{
-    // Copy from assignment 3
-}
+    }
+    int countCharacter (char *base , int size , char query )
+    {
+        int count  = 0 ;
+        for (int i = 0 ; i < size ; ++i )
+        {
+            if (base[i] == query )
+            {
+                ++ count ;
+            }
+        }
+        return count ;
+    }
 
 } // namespace dna
 
--- cpp-no-struct/shapes.hpp
+++ cpp-no-struct/shapes.hpp
@@ -3,53 +3,48 @@
 
 #include <cmath>
 
-namespace geometry
-{
+namespace geometry {
+    double squareArea(double side) {
+        return side * side;
+    }
 
-double squareArea(double side)
-{
-    return side * side;
-}
+    double rectangleArea(double width, double height)
+    {
+        return width * height;
+    }
 
-double rectangleArea(double width, double height)
-{
-    return width * height;
-}
+    double triangleArea(double a, double b, double c)
+    {
+        double s = (a + b + c) / 2;
+        return std::sqrt(s * (s - a) * (s - b) * (s - c));
+    }
 
-double triangleArea(double a, double b, double c)
-{
-    double s = (a + b + c) / 2;
-    return std::sqrt(s * (s - a) * (s - b) * (s - c));
-}
+    double circleArea(double radius)
+    {
+        return radius * radius * M_PI;
+    }
 
-double circleArea(double radius)
-{
-    return radius * radius * M_PI;
-}
+    bool circleContains(double centerX, double centerY, double radius, double testX, double testY)
+    {
+        double dx = centerX - testX;
+        double dy = centerY - testY;
+        return dx * dx + dy * dy <= radius * radius;
+    }
 
-bool circleContains(double centerX, double centerY, double radius, double testX, double testY)
-{
-    double dx = centerX - testX;
-    double dy = centerY - testY;
-    return dx * dx + dy * dy <= radius * radius;
-}
-
-bool squareContains(double upleftX, double upleftY, double length, double testX, double testY)
-{
-    return testX >= upleftX &&
-           testX <= upleftX + length &&
-           testY >= upleftY &&
-           testY <= upleftY + length;
-}
-
-bool rectangleContains(double upleftX, double upleftY, double downRightX, double downRightY,
-                       double testX, double testY)
-{
-    return testX >= upleftX &&
-           testX <= downRightX &&
-           testY >= upleftY &&
-           testY <= downRightY;
-}
+    bool squareContains(double upleftX, double upleftY, double length, double testX, double testY)
+    {
+        return testX >= upleftX &&
+        testX <= upleftX + length &&
+        testY >= upleftY &&
+        testY <= upleftY + length;
+    }
+    bool rectangleContains(double upleftX, double upleftY, double downRightX, double downRightY,double testX, double testY)
+    {
+        return testX >= upleftX &&
+        testX <= downRightX &&
+        testY >= upleftY &&
+        testY <= downRightY;
+    }
 
 } // namespace geometry
 
